dfe10a33d2759b036f966798ef4a1384a0a7820e
mjdude <mo@syncspot.net>
2017-05-05 16:07:51 -0700

    created timer

--- build/example_01_observables.js
+++ build/example_01_observables.js
@@ -6,46 +6,81 @@ var _Rx2 = _interopRequireDefault(_Rx);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var simple$ = new _Rx2.default.Observable(function (observer) {
-    console.log("Generating observable");
-    setTimeout(function () {
-        observer.next("An item");
-        setTimeout(function () {
-            observer.next("Another item!");
-            observer.complete();
-        }, 1000);
-    }, 1000);
-});
+// ---- PART 1 ----
 
-// first way to call subscribe
-simple$.subscribe(function (item) {
-    return console.log("one.next " + item);
-}, function (error) {
-    return console.log("one.error " + error);
-}, function () {
-    return console.log("one.complete");
-});
-
-// second way to call subscribe, pass in an object, also has 3 differnt ways to call
-// a function
-setTimeout(function () {
-    simple$.subscribe({
-        next: function next(item) {
-            return console.log("two.next " + item);
-        },
-        error: function error(_error) {
-            console.log("two.error " + _error);
-        },
-
-        complete: function complete() {
-            console.log("two.complete");
-        }
-    });
-}, 3000);
-
-// const promise = new Promise((resolve, reject) => {
-//     console.log('IN Promise')
-//     resolve('Hey')
+// const simple$ = new Rx.Observable((observer) => {
+//     console.log("Generating observable")
+//     setTimeout( () => {
+//         observer.next("An item")
+//         setTimeout(() => {
+//             observer.next("Another item!")
+//             observer.complete();
+//         }, 1000)
+//     }, 1000)
 // })
 
-// promise.then( item => console.log(item));
+// // first way to call subscribe
+// simple$.subscribe(
+//     item => console.log(`one.next ${item}`),
+//     error => console.log(`one.error ${error}`),
+//     () => console.log(`one.complete`)
+// );
+
+// // second way to call subscribe, pass in an object, also has 3 differnt ways to call
+// // a function
+// setTimeout(() => {
+//     simple$.subscribe({
+//         next: item => console.log(`two.next ${item}`),
+//         error(error){
+//             console.log(`two.error ${error}`)
+//         },
+//         complete: function(){
+//             console.log(`two.complete`)
+//         }
+//     })
+// }, 3000)
+
+// const error$ = new Rx.Observable(observer => {
+//     observer.error(new Error("Stuff check stack"))
+// })
+
+// // using error .stack on an error object gives us the stack trace
+// error$.subscribe(
+//     item => console.log(`one.next ${item}`),
+//     error => console.log(`one.error ${error.stack}`),
+//     () => console.log(`one.complete`)
+// );
+// // const promise = new Promise((resolve, reject) => {
+// //     console.log('IN Promise')
+// //     resolve('Hey')
+// // })
+
+// // promise.then( item => console.log(item));
+
+// ---- PART 2 ----
+
+function createSubscriber(tag) {
+    return {
+        next: function next(item) {
+            console.log(tag + ".next " + item);
+        },
+        error: function error(_error) {
+            console.log(tag + ".error " + (_error.stack || _error));
+        },
+        complete: function complete() {
+            console.log(tag + ".complete");
+        }
+    };
+}
+
+function createInterval$(time) {
+    return new _Rx2.default.Observable(function (observer) {
+        var index = 0;
+        setInterval(function () {
+            observer.next(index++);
+        }, time);
+    });
+}
+
+var everySecond$ = createInterval$(1000);
+everySecond$.subscribe(createSubscriber("one"));
--- src-server/example_01_observables.js
+++ src-server/example_01_observables.js
@@ -1,41 +1,75 @@
 import Rx from 'rxjs/Rx';
 
-const simple$ = new Rx.Observable((observer) => {
-    console.log("Generating observable")
-    setTimeout( () => {
-        observer.next("An item")
-        setTimeout(() => {
-            observer.next("Another item!")
-            observer.complete();
-        }, 1000)
-    }, 1000)
-})
+// ---- PART 1 ----
 
-// first way to call subscribe
-simple$.subscribe(
-    item => console.log(`one.next ${item}`),
-    error => console.log(`one.error ${error}`),
-    () => console.log(`one.complete`)
-);
-
-// second way to call subscribe, pass in an object, also has 3 differnt ways to call
-// a function
-setTimeout(() => {
-    simple$.subscribe({
-        next: item => console.log(`two.next ${item}`),
-        error(error){
-            console.log(`two.error ${error}`)
-        },
-        complete: function(){
-            console.log(`two.complete`)
-        }
-    })
-}, 3000)
-
-
-// const promise = new Promise((resolve, reject) => {
-//     console.log('IN Promise')
-//     resolve('Hey')
+// const simple$ = new Rx.Observable((observer) => {
+//     console.log("Generating observable")
+//     setTimeout( () => {
+//         observer.next("An item")
+//         setTimeout(() => {
+//             observer.next("Another item!")
+//             observer.complete();
+//         }, 1000)
+//     }, 1000)
 // })
 
-// promise.then( item => console.log(item));
+// // first way to call subscribe
+// simple$.subscribe(
+//     item => console.log(`one.next ${item}`),
+//     error => console.log(`one.error ${error}`),
+//     () => console.log(`one.complete`)
+// );
+
+// // second way to call subscribe, pass in an object, also has 3 differnt ways to call
+// // a function
+// setTimeout(() => {
+//     simple$.subscribe({
+//         next: item => console.log(`two.next ${item}`),
+//         error(error){
+//             console.log(`two.error ${error}`)
+//         },
+//         complete: function(){
+//             console.log(`two.complete`)
+//         }
+//     })
+// }, 3000)
+
+// const error$ = new Rx.Observable(observer => {
+//     observer.error(new Error("Stuff check stack"))
+// })
+
+// // using error .stack on an error object gives us the stack trace
+// error$.subscribe(
+//     item => console.log(`one.next ${item}`),
+//     error => console.log(`one.error ${error.stack}`),
+//     () => console.log(`one.complete`)
+// );
+// // const promise = new Promise((resolve, reject) => {
+// //     console.log('IN Promise')
+// //     resolve('Hey')
+// // })
+
+// // promise.then( item => console.log(item));
+
+// ---- PART 2 ----
+
+function createSubscriber(tag){
+    return {
+        next(item) {console.log(`${tag}.next ${item}`)},
+        error(error) {console.log(`${tag}.error ${error.stack || error}`)},
+        complete() {console.log(`${tag}.complete`)}
+    }
+}
+
+function createInterval$(time){
+    return new Rx.Observable(observer => {
+        let index = 0;
+        setInterval(() => {
+            observer.next(index++)
+        }, time)
+    })
+}
+
+
+const everySecond$ = createInterval$(1000);
+everySecond$.subscribe(createSubscriber("one"));
