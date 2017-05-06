5342e8b04f205f798cffe5da184d2dfee964f47b
mjdude <mo@syncspot.net>
2017-05-05 17:29:59 -0700

    added iterable example

--- build/example_02_built_in.js
+++ build/example_02_built_in.js
@@ -8,4 +8,16 @@ var _util = require('./lib/util');
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-_Rx2.default.Observable.interval(500).take(5).subscribe((0, _util.createSubscriber)("one"));
+// timer
+// Rx.Observable.interval(500)
+//     .take(5)
+//     .subscribe(createSubscriber("interval"));
+
+// timer
+// Rx.Observable.timer(5000, 500)
+//     .take(3)
+//     .subscribe(createSubscriber('timer'));
+
+_Rx2.default.Observable.of('Hello, world', 42, "whoa").subscribe((0, _util.createSubscriber)('of'));
+
+_Rx2.default.Observable.from([23, 10, 4]).subscribe((0, _util.createSubscriber)('from'));
--- src-server/example_02_built_in.js
+++ src-server/example_02_built_in.js
@@ -1,6 +1,18 @@
 import Rx from 'rxjs/Rx';
 import {createSubscriber} from './lib/util'; 
 
-Rx.Observable.interval(500)
-    .take(5)
-    .subscribe(createSubscriber("one"));
+// timer
+// Rx.Observable.interval(500)
+//     .take(5)
+//     .subscribe(createSubscriber("interval"));
+
+// timer
+// Rx.Observable.timer(5000, 500)
+//     .take(3)
+//     .subscribe(createSubscriber('timer'));
+
+Rx.Observable.of('Hello, world', 42, "whoa")
+    .subscribe(createSubscriber('of'))
+
+Rx.Observable.from([23, 10, 4])
+    .subscribe(createSubscriber('from'))
